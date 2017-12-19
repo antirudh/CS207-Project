c8db22a1c0956d429c672afa3c141012846b915c
antirudh <32561745+antirudh@users.noreply.github.com>
2017-12-18 20:36:11 -0600

    Add files via upload

---
+++ FinalSketch.ino
@@ -0,0 +1,126 @@
+#include <NewPing.h>  
+const int S0 =4;
+const int S1 =5;
+const int S2 =6;
+const int S3 =7;
+const int Sensor =12;
+int frequency =0;
+
+const int Motor1Pin1 = 8;
+const int Motor1Pin2 = 9;
+const int Motor2Pin2 =10;
+const int Motor2Pin1 = 11;
+const int trigPin = 3;
+const int echoPin = 2;
+// defines variables
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+ pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(Sensor, INPUT);
+}
+void loop() {
+
+   digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  // mapping the frequency value to the RGB model of 0 to 255.
+  frequency = map(frequency, 25,72,255,0);
+  if (frequency < 25)
+  {
+    movestop();
+  }
+    // Setting Green filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  frequency = map(frequency, 30,90,255,0);
+  if (frequency > 30)
+  {
+    moveForward();
+  }
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+if (distance < 25)
+{
+  moveStop();
+  delay(1000);
+  moveBackward();
+   delay(1000);
+  moveleft();
+   delay(1000);
+ 
+
+ 
+}
+else
+{
+  moveForward();
+}
+
+ 
+
+ 
+}
+
+void moveBackward(){
+  digitalWrite(Motor1Pin1, HIGH);
+  digitalWrite(Motor1Pin2, LOW);
+ digitalWrite(Motor2Pin1, HIGH);
+  digitalWrite(Motor2Pin2, LOW);
+  
+  
+}
+
+
+void moveForward(){
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, HIGH);
+  digitalWrite(Motor2Pin1, LOW);
+  digitalWrite(Motor2Pin2, HIGH);
+}
+
+
+void moveleft(){
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, HIGH);
+  digitalWrite(Motor2Pin1, HIGH);
+  digitalWrite(Motor2Pin2, LOW);
+}
+
+
+void moveright(){
+  digitalWrite(Motor1Pin1, HIGH);
+  digitalWrite(Motor1Pin2, LOW);
+  digitalWrite(Motor2Pin1, LOW); 
+  digitalWrite(Motor2Pin2, HIGH);
+}
+
+
+void moveStop(){
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, LOW);
+  digitalWrite(Motor2Pin1, LOW);
+  digitalWrite(Motor2Pin2, LOW);
+}
+
